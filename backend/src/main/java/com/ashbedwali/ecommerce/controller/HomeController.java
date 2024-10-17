b7be2595b5b0182c197491eb68d13a6e30749018
Ashler18 <delekeashler@gmail.com>
2024-10-17 21:46:36 +0200

    Message de commit ici

---
+++ backend/src/main/java/com/ashbedwali/ecommerce/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ashbedwali.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Bienvenue sur l'application e-commerce de voitures !";
+    }
+}
