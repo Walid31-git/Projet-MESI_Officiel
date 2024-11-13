c7821281906fe2b20869a69211adf4c1db5d0c8c
ALLAL Walid <ghoutiallal@gmail.com>
2024-11-13 13:25:34 +0100

    Ajout de l'image et root

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PageAcceuilComponent } from './page-acceuil/page-acceuil.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PageAcceuilComponent },
+];
