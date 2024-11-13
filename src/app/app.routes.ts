431a5a176c3a0dd24c913c8d02cb1583306d3bf3
ALLAL Walid <ghoutiallal@gmail.com>
2024-11-13 17:37:31 +0100

    Ajout de La page contact et la page de vente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { PageAcceuilComponent } from './page-acceuil/page-acceuil.component';
+import { PageVenteComponent } from './page-vente/page-vente.component';
+import { PageContactComponent } from './page-contact/page-contact.component';
+
 import path from 'path';
 
 export const routes: Routes = [
     { path: '', component: PageAcceuilComponent },
+    { path: 'achat', component: PageAcceuilComponent },
+    { path: 'vente', component: PageVenteComponent },
+    { path: 'contact', component: PageContactComponent }
 ];
