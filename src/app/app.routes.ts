0a6762f63dd74d45fb8f2f56ea11c05b05676375
Abdelhadi Alioui <abdelhadialioui2003@gmail.com>
2024-11-15 09:53:01 +0100

    Ajout de la page Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,19 @@
+
+
 import { Routes } from '@angular/router';
 import { PageAcceuilComponent } from './page-acceuil/page-acceuil.component';
-<<<<<<< HEAD
-import { LoginComponent } from './login/login.component';  
-
-export const routes: Routes = [
-    { path: '', component: PageAcceuilComponent },
-    { path: 'login', component: LoginComponent}
-=======
 import { PageVenteComponent } from './page-vente/page-vente.component';
 import { PageContactComponent } from './page-contact/page-contact.component';
 
 import path from 'path';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', component: PageAcceuilComponent },
+    { path: 'Login', component: LoginComponent},
     { path: 'achat', component: PageAcceuilComponent },
     { path: 'vente', component: PageVenteComponent },
     { path: 'contact', component: PageContactComponent }
->>>>>>> 431a5a176c3a0dd24c913c8d02cb1583306d3bf3
 ];
 
 
