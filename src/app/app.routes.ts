f85562559567b883c336e5782a0329c6d3404238
Abdelhadi Alioui <abdelhadialioui2003@gmail.com>
2024-11-15 09:25:28 +0100

    Ajout de pages CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { PageAcceuilComponent } from './page-acceuil/page-acceuil.component';
-import path from 'path';
+import { LoginComponent } from './login/login.component';  
 
 export const routes: Routes = [
     { path: '', component: PageAcceuilComponent },
+    { path: 'login', component: LoginComponent}
 ];
+
+
